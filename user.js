468666584d5caa6ee961282e30faf736f38dcb8e
alstjr7375 <alstjr7375@daum.net>
2021-06-01 10:17:49 +0900

    Add: Compact Mode - Restore

--- user.js
+++ user.js
@@ -2,7 +2,10 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // Fill SVG Color
-user_pref("svg.context-properties.content.enabled", true); 
+user_pref("svg.context-properties.content.enabled", true);
 
 // CSS Blur Filter - 88 Above
 user_pref("layout.css.backdrop-filter.enabled", true);
+
+// Restore Compact Mode - 89 Above
+user_pref("browser.compactmode.show", true);
