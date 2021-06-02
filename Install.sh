17e261f80580cd94ac590390fc58e098027b2b27
7k5x <computasaesum@gmail.com>
2021-06-03 00:35:33 +0900

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,19 +1,18 @@
-autocopy() {
-  local file=${1}
-  local target=${2}
-
-  if [ -e "${target}" ]; then
-    echo "${target} alreay exist."
-    echo "Now Backup.."
-    autocopy "${target}" "${target}.bak"
-    echo""
-  fi
-
-  cp -v "${file}" "${target}"
-}
-
-install_lepton() {
-  local userConfig="user.js"
-  autocopy "${userConfig}" "../${userConfig}"
-}
-install_lepton
+#!/usr/bin/env bash
+#Shell scripts in general should not use functions too much since they could slow down the code greatly.
+#What I mean my that is write spagetti shell scripts
+#TODO(by black7375)
+#However, there may be existing user's settings, so back up and make a copy.
+#I just made a simple one yesterday, assuming that there may be multiple backup states.
+#I think it's better to do a redirection(>>) after backup to merge the contents of the file.
+printf "Are you using ESR, Default, or Dev?(ESR/Default/Dev): "
+read
+if [ "ESR" == "$REPLY" ] || [ "$REPLY" == "esr" ]; then
+  profiledir="$(grep '.default-esr' ~/.mozilla/firefox/profiles.ini | grep 'Default=' | cut -f 2 -d'=')"
+elif [ "Default" == "$REPLY" ] || [ "default" == "$REPLY" ]; then
+  profiledir="$(grep '.default-release' ~/.mozilla/firefox/profiles.ini | grep 'Default=' | cut -f 2 -d'=')"
+elif [ "Dev" == "$REPLY" ] || [ "dev" == "$REPLY" ]; then
+  profiledir="$(grep '.dev-edition-default' ~/.mozilla/firefox/profiles.ini | grep 'Default=' | cut -f 2 -d'=')"
+else
+  echo "What did you say?"
+  break
