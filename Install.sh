9d2591d64b04b8b22d768b735fec0f500c4cc8f5
7k5x <computasaesum@gmail.com>
2021-06-03 09:59:57 +0900

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#Shell scripts in general should not use functions too much since they could slow down the code greatly.
-#What I mean my that is write spagetti shell scripts
 #TODO(by black7375)
 #However, there may be existing user's settings, so back up and make a copy.
 #I just made a simple one yesterday, assuming that there may be multiple backup states.
@@ -16,12 +14,14 @@ elif [ "Dev" == "$REPLY" ] || [ "dev" == "$REPLY" ]; then
 else
   echo "Unspecified."
   exit
+fi
 printf "Do you want to disable everything being selected when you click at the URL bar?(N/y): "
 read
 if [ "y" == "$REPLY" ] || [ "Y" == "$REPLY" ]; then
   curl -s -L https://raw.githubusercontent.com/7k5x/firefox-selection-fix/master/fixfx-selection.sh | bash
 else
   printf "\nSkipiing.\n"
+fi
 if [[ -f ~/.mozilla/Firefox/${profiledir}/user.js ]]
 then
     printf "user.js exists. Do you want to make a backup of it?(Y/n): "
@@ -36,32 +36,37 @@ then
       then
         rm -rf ~/.mozilla/Firefox/${profiledir}/backup
       fi
-      mkdir ~/.mozilla/Firefox/${profiledir}/backup
-      mv ~/.mozilla/Firefox/${profiledir}/user.js ~/.mozilla/Firefox/${profiledir}/backup
+    fi
+    else
       cp user.js ~/.mozilla/Firefox/${profiledir}
-      
 fi
-cp user.js ~/.mozilla/Firefox/${profiledir}
 if [[ -d "~/.mozilla/Firefox/${profiledir}/chrome" ]]
 then
-  printf "user.js exists. Do you want to make a backup of it?(Y/n): "
-    read
-    if [ "N" == "$REPLY" ] || [ "n" == "$REPLY" ]; then
-      printf"Overwriting...\n"
-      rm -rf ~/.mozilla/Firefox/${profiledir}/user.js
-      cp user.js ~/.mozilla/Firefox/${profiledir}
-    else
-      printf "Making a backup...\n"
-      if [[ -d "~/.mozilla/Firefox/${profiledir}/backup" ]]
-      then
-        rm -rf ~/.mozilla/Firefox/${profiledir}/backup
-      fi
-      mkdir ~/.mozilla/Firefox/${profiledir}/backup
-      mv ~/.mozilla/Firefox/${profiledir}/user.js ~/.mozilla/Firefox/${profiledir}/backup
-      cp user.js ~/.mozilla/Firefox/${profiledir}
-      
+  printf "The directory chrome/ exists. Do you want to make a backup of it?(Y/n): "
+  read
+  if [ "N" == "$REPLY" ] || [ "n" == "$REPLY" ]; then
+    printf"Overwriting...\n"
+    rm -rf ~/.mozilla/Firefox/${profiledir}/chrome
+    mkdir ~/.mozilla/Firefox/${profiledir}/chrome
+    cp userChrome.css ~/.mozilla/Firefox/${profiledir}/chrome
+    cp userContent.css ~/.mozilla/Firefox/${profiledir}/chrome
+    cp -r icon ~/.mozilla/Firefox/${profiledir}/chrome
+  else
+    printf "Making a backup...\n"
+    if [[ -d "~/.mozilla/Firefox/${profiledir}/backup/chrome" ]]
+    then
+      rm -rf ~/.mozilla/Firefox/${profiledir}/backup/chrome
+    fi
+    mkdir ~/.mozilla/Firefox/${profiledir}/backup/chrome
+    mv ~/.mozilla/Firefox/${profiledir}/chrome ~/.mozilla/Firefox/${profiledir}/backup
+    cp userChrome.css ~/.mozilla/Firefox/${profiledir}/chrome
+    cp userContent.css ~/.mozilla/Firefox/${profiledir}/chrome
+    cp -r icon ~/.mozilla/Firefox/${profiledir}/chrome
+  fi
+  else
+    mkdir ~/.mozilla/Firefox/${profiledir}/chrome
+    cp userChrome.css ~/.mozilla/Firefox/${profiledir}/chrome
+    cp userContent.css ~/.mozilla/Firefox/${profiledir}/chrome
+    cp -r icon ~/.mozilla/Firefox/${profiledir}/chrome
 fi
-mkdir ~/.mozilla/Firefox/${profiledir}/chrome
-cp userChrome.css ~/.mozilla/Firefox/${profiledir}/chrome
-cp userContent.css ~/.mozilla/Firefox/${profiledir}/chrome
-cp -r icon ~/.mozilla/Firefox/${profiledir}/chrome
+printf "\nInstallation finished."
