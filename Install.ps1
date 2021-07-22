38011804d956d8bff80a40815a944e6cd4534eba
alstjr7375 <alstjr7375@daum.net>
2021-07-22 10:43:00 +0900

    Clean: Installer - Change to multiline

--- Install.ps1
+++ Install.ps1
@@ -64,7 +64,9 @@ function Lepton-OKMessage() {
 #== Required Tools =============================================================
 function Install-Choco() {
   # https://chocolatey.org/install
-  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  Set-ExecutionPolicy Bypass -Scope Process -Force
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 function Check-Git() {
