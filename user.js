515c3bc3a9bba0d3e93b9dbb3e07415746275b83
alstjr7375 <alstjr7375@daum.net>
2022-03-18 11:50:14 +0900

    Clean: Change opation variables to literal (selected tab)

--- user.js
+++ user.js
@@ -34,8 +34,8 @@ user_pref("userChrome.tab.connect_to_window",       true); // Original, Photon
 user_pref("userChrome.tab.box_shadow",              true); // Original
 user_pref("userChrome.tab.bottom_rounded_corner",   true); // Original
 user_pref("userChrome.tab.lepton_like_padding",     true); // Original
-// user_pref("userChrome.tab.photon_like_contextline", true); // Photon
 // user_pref("userChrome.tab.photon_like_padding",  true); // Photon
+// user_pref("userChrome.tab.photon_like_contextline", true); // Photon
 // user_pref("userChrome.tab.photon_like_radius",   true); // Photon
 
 user_pref("userChrome.panel.proton", true); // Original, Proton
