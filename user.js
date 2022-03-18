5302555be2546dcaa00f3a070ccfc23ab5e8eeba
alstjr7375 <alstjr7375@daum.net>
2022-03-18 16:21:57 +0900

    Clean: user.js option, indent

--- user.js
+++ user.js
@@ -27,29 +27,25 @@ user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar"
 // user_pref("layout.css.prefers-color-scheme.content-override", 3);
 
 // ** Theme Related Options ****************************************************
-user_pref("userChrome.tab.original",                true); // Original
-// user_pref("userChrome.tab.photon",               true); // Photon
-// user_pref("userChrome.tab.proton",               true); // Proton
-
 // Mutliple
-user_pref("userChrome.tab.connect_to_window", true); // Original, Photon
+user_pref("userChrome.tab.connect_to_window",          true); // Original, Photon
 
-user_pref("userChrome.panel.proton",    true); // Original, Proton
-// user_pref("userChrome.panel.photon", true); // Photon
+user_pref("userChrome.panel.proton",                   true); // Original, Proton
+// user_pref("userChrome.panel.photon",                true); // Photon
 
-user_pref("userChrome.tab.lepton_like_padding",    true); // Original
-// user_pref("userChrome.tab.photon_like_padding", true); // Photon
+user_pref("userChrome.tab.lepton_like_padding",        true); // Original
+// user_pref("userChrome.tab.photon_like_padding",     true); // Photon
 
-user_pref("userChrome.tab.dynamic_separtor",    true); // Original, Proton
-// user_pref("userChrome.tab.static_separator", true); // Photon
+user_pref("userChrome.tab.dynamic_separtor",           true); // Original, Proton
+// user_pref("userChrome.tab.static_separator",        true); // Photon
 
-user_pref("userChrome.tab.newtab_button_like_tab",   true); // Original
-// user_pref("userChrome.tab.newtab_button_smaller", true); // Photon
-// user_pref("userChrome.tab.newtab_button_proton",  true); // Proton
+user_pref("userChrome.tab.newtab_button_like_tab",     true); // Original
+// user_pref("userChrome.tab.newtab_button_smaller",   true); // Photon
+// user_pref("userChrome.tab.newtab_button_proton",    true); // Proton
 
 // Original Only
-user_pref("userChrome.tab.box_shadow",              true);
-user_pref("userChrome.tab.bottom_rounded_corner",   true);
+user_pref("userChrome.tab.box_shadow",                 true);
+user_pref("userChrome.tab.bottom_rounded_corner",      true);
 
 // Photon Only
 // user_pref("userChrome.tab.photon_like_contextline", true);
@@ -60,33 +56,34 @@ user_pref("userChrome.tab.bottom_rounded_corner",   true);
 // user_pref("userChrome.padding.drag_space", true);
 
 // == Theme Default Settings ===================================================
-user_pref("userChrome.compatibility.theme", true);
-user_pref("userChrome.compatibility.os",    true);
+// -- User Chrome --------------------------------------------------------------
+user_pref("userChrome.compatibility.theme",       true);
+user_pref("userChrome.compatibility.os",          true);
 
-user_pref("userChrome.theme.built_in_contrast", true);
-user_pref("userChrome.theme.system_default",    true);
-user_pref("userChrome.theme.proton_color",      true);
-user_pref("userChrome.theme.proton_chrome",     true); // Need proton_color
-user_pref("userChrome.theme.fully_color",       true); // Need proton_color
-user_pref("userChrome.theme.fully_dark",        true); // Need proton_color
+user_pref("userChrome.theme.built_in_contrast",   true);
+user_pref("userChrome.theme.system_default",      true);
+user_pref("userChrome.theme.proton_color",        true);
+user_pref("userChrome.theme.proton_chrome",       true); // Need proton_color
+user_pref("userChrome.theme.fully_color",         true); // Need proton_color
+user_pref("userChrome.theme.fully_dark",          true); // Need proton_color
 
 user_pref("userChrome.decoration.cursor",         true);
 user_pref("userChrome.decoration.field_border",   true);
 user_pref("userChrome.decoration.download_panel", true);
 user_pref("userChrome.decoration.animate",        true);
 
-user_pref("userChrome.padding.tabbar_width",   true);
-user_pref("userChrome.padding.tabbar_height",  true);
-user_pref("userChrome.padding.toolbar_button", true);
-user_pref("userChrome.padding.navbar_width",   true);
-user_pref("userChrome.padding.urlbar",         true);
-user_pref("userChrome.padding.bookmarkbar",    true);
-user_pref("userChrome.padding.infobar",        true);
-user_pref("userChrome.padding.menu",           true);
-user_pref("userChrome.padding.bookmark_menu",  true);
-user_pref("userChrome.padding.global_menu",    true);
-user_pref("userChrome.padding.panel",          true);
-user_pref("userChrome.padding.popup_panel",    true);
+user_pref("userChrome.padding.tabbar_width",      true);
+user_pref("userChrome.padding.tabbar_height",     true);
+user_pref("userChrome.padding.toolbar_button",    true);
+user_pref("userChrome.padding.navbar_width",      true);
+user_pref("userChrome.padding.urlbar",            true);
+user_pref("userChrome.padding.bookmarkbar",       true);
+user_pref("userChrome.padding.infobar",           true);
+user_pref("userChrome.padding.menu",              true);
+user_pref("userChrome.padding.bookmark_menu",     true);
+user_pref("userChrome.padding.global_menu",       true);
+user_pref("userChrome.padding.panel",             true);
+user_pref("userChrome.padding.popup_panel",       true);
 
 user_pref("userChrome.tab.multi_selected",        true);
 user_pref("userChrome.tab.unloaded",              true);
@@ -98,14 +95,15 @@ user_pref("userChrome.tab.pip",                   true);
 user_pref("userChrome.tab.container",             true);
 user_pref("userChrome.tab.crashed",               true);
 
-user_pref("userChrome.fullscreen.overlap", true);
+user_pref("userChrome.fullscreen.overlap",        true);
 
-user_pref("userChrome.icon.library",      true);
-user_pref("userChrome.icon.panel",        true);
-user_pref("userChrome.icon.menu",         true);
-user_pref("userChrome.icon.context_menu", true);
-user_pref("userChrome.icon.global_menu",  true);
+user_pref("userChrome.icon.library",              true);
+user_pref("userChrome.icon.panel",                true);
+user_pref("userChrome.icon.menu",                 true);
+user_pref("userChrome.icon.context_menu",         true);
+user_pref("userChrome.icon.global_menu",          true);
 
+// -- User Content -------------------------------------------------------------
 user_pref("userContent.player.ui",            true);
 user_pref("userContent.player.icon",          true);
 user_pref("userContent.player.noaudio",       true);
@@ -113,15 +111,15 @@ user_pref("userContent.player.size",          true);
 user_pref("userContent.player.click_to_play", true);
 user_pref("userContent.player.animate",       true);
 
-user_pref("userContent.newTab.field_border", true);
-user_pref("userContent.newTab.full_icon",    true);
-user_pref("userContent.newTab.animate",      true);
-user_pref("userContent.newTab.searchbar",    true);
+user_pref("userContent.newTab.field_border",  true);
+user_pref("userContent.newTab.full_icon",     true);
+user_pref("userContent.newTab.animate",       true);
+user_pref("userContent.newTab.searchbar",     true);
 
-user_pref("userContent.page.illustration", true);
-user_pref("userContent.page.proton_color", true);
-user_pref("userContent.page.dark_mode",    true); // Need proton_color
-user_pref("userContent.page.proton",       true); // Need proton_color
+user_pref("userContent.page.illustration",    true);
+user_pref("userContent.page.proton_color",    true);
+user_pref("userContent.page.dark_mode",       true); // Need proton_color
+user_pref("userContent.page.proton",          true); // Need proton_color
 
 // ** Useful Options ***********************************************************
 // Integrated calculator at urlbar
