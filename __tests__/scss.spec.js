f7d5ff2c201a9fe9c0c03a75044ec26d610b24cb
alstjr7375 <alstjr7375@daum.net>
2022-03-20 08:04:56 +0900

    Fix: Test - Run each test

--- __tests__/scss.spec.js
+++ __tests__/scss.spec.js
@@ -3,13 +3,11 @@ const sassTrue = require("sass-true");
 const glob = require("glob");
 
 // https://www.educative.io/blog/sass-tutorial-unit-testing-with-sass-true
-describe("Sass", () => {
-  // Find all of the Sass files that end in `*.test.scss` in any directory of this project.
-  // I use path.resolve because True requires absolute paths to compile test files.
-    const sassTestFiles = glob.sync(path.resolve(process.cwd(), "__tests__/**/*.test.scss"));
+// Find all of the Sass files that end in `*.test.scss` in any directory of this project.
+// I use path.resolve because True requires absolute paths to compile test files.
+const sassTestFiles = glob.sync(path.resolve(process.cwd(), "__tests__/**/*.test.scss"));
 
   // Run True on every file found with the describe and it methods provided
-  sassTestFiles.forEach(file =>
-    sassTrue.runSass({ file }, { describe, it })
-  );
+sassTestFiles.forEach(file => {
+    describe(file, () => sassTrue.runSass({ file }, { describe, it }));
 });
