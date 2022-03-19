4b1bc67f594f748179661e2f21ffd16e1282912a
alstjr7375 <alstjr7375@daum.net>
2022-03-20 08:56:25 +0900

    Fix: Test - `glob` -> `fast-glob`

--- __tests__/scss.spec.js
+++ __tests__/scss.spec.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const sassTrue = require("sass-true");
-const glob = require("glob");
+const glob = require("fast-glob");
 
 // https://www.educative.io/blog/sass-tutorial-unit-testing-with-sass-true
 // Find all of the Sass files that end in `*.test.scss` in any directory of this project.
 // I use path.resolve because True requires absolute paths to compile test files.
-const sassTestFiles = glob.sync(path.resolve(process.cwd(), "__tests__/**/*.test.scss"));
+const pattern = path.resolve(process.cwd(), "__tests__/**/*.test.scss");
+const sassTestFiles = glob.sync([pattern]);
 
-  // Run True on every file found with the describe and it methods provided
+// Run True on every file found with the describe and it methods provided
 sassTestFiles.forEach(file => {
-    describe(file, () => sassTrue.runSass({ file }, { describe, it }));
+  describe(file, () => sassTrue.runSass({ file }, { describe, it }));
 });
