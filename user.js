749530fae44e2eed75bbcaf5ca69a9a24514c6fe
alstjr7375 <alstjr7375@daum.net>
2022-03-21 15:45:00 +0900

    Fix: Option - change `always_show_close_button_at_hover` -> `close_button_at_hover.always`

--- user.js
+++ user.js
@@ -62,10 +62,10 @@ user_pref("userChrome.tab.bottom_rounded_corner",      true);
 // user_pref("userChrome.urlView.go_button_when_typing",    true);
 // user_pref("userChrome.urlView.always_show_page_actions", true);
 
-// user_pref("userChrome.tab.always_show_tab_icon",              true);
-// user_pref("userChrome.tab.always_show_close_button_at_hover", true); // Need close_button_at_hover
-// user_pref("userChrome.tab.sound_show_label",                  true); // Need remove sound_hide_label
-// user_pref("userChrome.tab.centered_label",                    true);
+// user_pref("userChrome.tab.always_show_tab_icon",         true);
+// user_pref("userChrome.tab.close_button_at_hover.always", true); // Need close_button_at_hover
+// user_pref("userChrome.tab.sound_show_label",             true); // Need remove sound_hide_label
+// user_pref("userChrome.tab.centered_label",               true);
 
 // == Theme Default Settings ===================================================
 // -- User Chrome --------------------------------------------------------------
