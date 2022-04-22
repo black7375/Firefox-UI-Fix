8801366f2705aecb4f9791b0fe1c00ef9c1d330b
alstjr7375 <alstjr7375@daum.net>
2022-04-23 01:41:47 +0900

    Fix: Install - choco path

--- install.ps1
+++ install.ps1
@@ -99,7 +99,7 @@ function Install-Choco() {
   Set-ExecutionPolicy Bypass -Scope Process -Force
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
   iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-  $env:Path += ";C:\ProgramData\chocoportable'"
+  $env:Path += ";C:\ProgramData\chocoportable"
 }
 
 function Check-Git() {
