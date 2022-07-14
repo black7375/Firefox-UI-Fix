9d5ba38ab27509512e1022ae8a2285f9dac7cae5
alstjr7375 <alstjr7375@daum.net>
2022-07-14 10:23:34 +0900

    Add: Auto Hide - Bookmark Bar

--- user.js
+++ user.js
@@ -70,6 +70,7 @@ user_pref("userChrome.tab.bottom_rounded_corner",      true);
 
 // user_pref("userChrome.theme.proton_color.dark_blue_accent", true);
 
+// user_pref("userChrome.autohide.bookmarkbar",                true);
 // user_pref("userChrome.autohide.forward_button",             true);
 // user_pref("userChrome.autohide.sidebar",                    true);
 
