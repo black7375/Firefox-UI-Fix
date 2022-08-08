b6adcce94f85b6c1aec1df0b0bfd0daac1dfdd90
alstjr7375 <alstjr7375@daum.net>
2022-08-08 11:10:55 +0900

    Fix: Centered - Replace centered options
    - `userChrome.tab.centered_content` -> `userChrome.centered.tab`
    - `userChrome.tab.centered_label` -> `userChrome.centered.tab.label`
    - `userChrome.bookmarkbar.centered` -> `userChrome.centered.bookmarkbar`

--- user.js
+++ user.js
@@ -93,6 +93,10 @@ user_pref("userChrome.rounding.square_tab",           false);
 // user_pref("userChrome.hidden.selected_tab",                 true);
 // user_pref("userChrome.hidden.disabled_menu",                true);
 
+// user_pref("userChrome.centered.tab",                        true);
+// user_pref("userChrome.centered.tab.label",                  true);
+// user_pref("userChrome.centered.bookmarkbar",                true);
+
 // user_pref("userChrome.rounding.square_button",              true);
 // user_pref("userChrome.rounding.square_panel",               true);
 // user_pref("userChrome.rounding.square_panelitem",           true);
@@ -130,10 +134,6 @@ user_pref("userChrome.rounding.square_tab",           false);
 // user_pref("userChrome.tab.close_button_at_pinned.background", true);
 // user_pref("userChrome.tab.close_button_at_hover.always",    true); // Need close_button_at_hover
 // user_pref("userChrome.tab.sound_show_label",                true); // Need remove sound_hide_label
-// user_pref("userChrome.tab.centered_content",                true);
-// user_pref("userChrome.tab.centered_label",                  true);
-
-// user_pref("userChrome.bookmarkbar.centered",                true);
 
 // user_pref("userChrome.panel.remove_strip",                  true);
 // user_pref("userChrome.panel.full_width_separator",          true);
