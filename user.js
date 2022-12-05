143790b3593beaacd99945b328be6ecd234f05a1
alstjr7375 <alstjr7375@daum.net>
2022-12-06 02:35:54 +0900

    Fix: Contents - Unify `newTab.field_border` -> `page.field_border`

--- user.js
+++ user.js
@@ -222,12 +222,12 @@ user_pref("userContent.player.size",           true);
 user_pref("userContent.player.click_to_play",  true);
 user_pref("userContent.player.animate",        true);
 
-user_pref("userContent.newTab.field_border",   true);
 user_pref("userContent.newTab.full_icon",      true);
 user_pref("userContent.newTab.animate",        true);
 user_pref("userContent.newTab.pocket_to_last", true);
 user_pref("userContent.newTab.searchbar",      true);
 
+user_pref("userContent.page.field_border",     true);
 user_pref("userContent.page.illustration",     true);
 user_pref("userContent.page.proton_color",     true);
 user_pref("userContent.page.dark_mode",        true); // Need proton_color
