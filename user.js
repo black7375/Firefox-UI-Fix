6ee9df6f9e5d925e96a93688ae9c52b71f3112c7
alstjr7375 <alstjr7375@daum.net>
2023-04-04 08:26:45 +0900

    Add: Theme - Linux non native menu without icons

--- user.js
+++ user.js
@@ -71,6 +71,7 @@ user_pref("userChrome.rounding.square_tab",           false);
 // user_pref("userChrome.theme.private",                       true);
 // user_pref("userChrome.theme.proton_color.dark_blue_accent", true);
 // user_pref("userChrome.theme.monospace",                     true);
+// user_pref("userChrome.theme.non_native_menu",               true); // only for linux
 // user_pref("userChrome.compatibility.os.win11",              true);
 
 // user_pref("userChrome.decoration.disable_panel_animate",    true);
