fbcd4cd954d1bab6ae919ce78f8812e55abbb715
alstjr7375 <alstjr7375@daum.net>
2023-04-16 07:36:49 +0900

    Add: Theme - Transparent menu #585

--- user.js
+++ user.js
@@ -72,6 +72,7 @@ user_pref("userChrome.rounding.square_tab",           false);
 // user_pref("userChrome.theme.proton_color.dark_blue_accent", true);
 // user_pref("userChrome.theme.monospace",                     true);
 // user_pref("userChrome.theme.transparent.frame",             true);
+// user_pref("userChrome.theme.transparent.menu",              true);
 // user_pref("userChrome.theme.non_native_menu",               true); // only for linux
 // user_pref("userChrome.compatibility.os.win11",              true);
 
